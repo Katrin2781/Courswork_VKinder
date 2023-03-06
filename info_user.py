4757abe92ce423e18204e75088490386a67d9a6e
Alex Bogatyrev <a.bogatyrev123@gmail.com>
2023-03-06 21:45:39 +0300

    edited_4

--- info_user.py
+++ info_user.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import date
+from random import randrange
 from DB_vkinder import insert_user
+from config_read import perstoken,bottoken
 
 class VkDownloader():
 
@@ -17,15 +19,13 @@ class VkDownloader():
             'v': 5.131
         }
         res = requests.get(url_info, params=params).json()
-
         for value in res["response"]:
             city = value['city']
             sex = value['sex']
             bdate = value['bdate']
             name = value['first_name'] + ' ' + value['last_name']
             user_list = [city, sex, name, bdate, user_id]
-
-            insert_user(user_list)
+            # insert_user(user_list)
             return user_list
 
     # Используется только Токен персональный
@@ -42,7 +42,7 @@ class VkDownloader():
         year = int(get_year[2])
         age = today.year - year - ((today.month, today.day) < (month, day))
         params = {
-            'count': 3,
+            'count': 10,
             'fields': 'photo_max',
             'city': data[0]["id"],
             'sex': sex,
@@ -52,9 +52,14 @@ class VkDownloader():
             'v': 5.131
             }
         res = requests.get(url_info, params=params).json()
+
         return res
+        
 
     def get_photo(self, id):#функция выгрузки фото с профиля
+        photo_all =[]
+        photo_3 = []
+        num = []
         url_photo = 'https://api.vk.com/method/photos.getProfile'
         params = {
             'owner_id': id,
@@ -65,34 +70,67 @@ class VkDownloader():
             'v': 5.131
         }
         res = requests.get(url_photo, params=params).json()
-
-        photo_all =[]
-        photo_3 = []
         #находим ссылки на фото и количество лайков, создаем словари и добавляем их в списко
         for values in res["response"]["items"]:
             file_likes = values["likes"]["count"]
             id_photo = values["id"]
             file_photo = {'id_photo': id_photo, 'likes': file_likes}
             photo_all.append(file_photo)
-
         #все лайки переношу в список, сортирую, оставляю 3 самых больших значения
-        num = []
         for links in photo_all:
             num.append(links['likes'])
-        num.sort()
-        num = [num[-1], num[-2], num[-3]]
-        #нахожу id на фото по 3 максимальным значениям(умнее не придумал)
-
+        n = min(3, len(num))
+        max_numbers = sorted(num, reverse=True)[:n]
         for links in photo_all:
-            if links['likes'] == num[0]:
+            if links['likes'] in max_numbers:
                 photo_3.append(links['id_photo'])
-            elif links['likes'] == num[1]:
-                photo_3.append(links['id_photo'])
-            elif links['likes'] == num[2]:
-                photo_3.append(links['id_photo'])
-
         return photo_3
+    
+    
+    def get_profile_3(self, user_id):#функция для вызова всех функций
+        user_list = self.user_info(user_id)#
+        get_info_3 = self.user_search(user_list)#
+        profile_3 = []
+        for values in get_info_3["response"]["items"]:
+            id_person = values["id"]
+            photo_profile = self.get_photo(id_person)
+            attachment = []
+            for photo in photo_profile:
+                attachment_one = f'photo{id_person}_{photo}'
+                attachment.append(attachment_one)
+            attachment = ','.join(attachment)
+            link_id = f'https://vk.com/id{id_person}'
+            name = values["first_name"] + ' ' + values["last_name"]
+            profile = {'id': id_person, 'name': name, 'link_id': link_id, 'attachment': attachment, 'user_id': user_list[4]}#
+            profile_3.append(profile)
+        return profile_3#словарь с данными 1000 человеков)
+    
 
-
+    def extract_random(self, lst):
+        if not lst:
+            return None
+        index = randrange(len(lst))
+        return lst.pop(index)
+    
+    
+    def message_send_photo(self, user_id, bottoken):#функция принимает токен группы и словарь с данными людей
+        profile_3 = self.get_profile_3(user_id)
+        user_info = self.extract_random(profile_3)
+        for info in user_info:#распаковываем словарь, присваивая значения из него
+            user_id = info['user_id']#ваш id
+            name = info['name']#имя и фамилия челвоека
+            link = info['link_id']#ссылка на его профиль
+            message = f'{name}\n {link}'
+            att = info['attachment']
+            url_photo = 'https://api.vk.com/method/messages.send'
+            params = {
+                'user_id': user_id,
+                'random_id': randrange(10 ** 7),
+                'message': message,# имя, фамилия и ссылка на профиль
+                'attachment': att,#для вывода 3 фоток сразу
+                'access_token': bottoken,
+                'v': 5.131
+            }
+            requests.get(url_photo, params=params)#печатаем в чат
 
 
