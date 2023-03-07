15c600c6fe6d747e7b47523c99e6be921641ce09
Yuri Shornikov <dbonlyfun@gmail.com>
2023-03-08 01:16:10 +0300

    button

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from random import randrange
 from vk_api.longpoll import VkLongPoll, VkEventType
 from info_user import VkDownloader
 from config_read import bottoken, perstoken
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
 vk = vk_api.VkApi(token=bottoken)
 longpoll = VkLongPoll(vk)
 
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+def write_msg(user_id, message, keyboard=None):
+    start = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': randrange(10 ** 7),
+    }
+
+    if keyboard != None:
+        start['keyboard'] = keyboard.get_keyboard()
+    else:
+        start = start
+
+    vk.method('messages.send', start)
     
 
 for event in longpoll.listen():
@@ -23,12 +36,24 @@ for event in longpoll.listen():
             request = event.text
 
             if request == "привет":
-
-                write_msg(event.user_id, f"Хай, {event.user_id}. Хоте ли бы вы познакомиться с новыми людьми? Напишите да/нет")
-            elif request == "да":
+                keyboard = VkKeyboard(one_time=True)
+                keyboard.add_button('start', VkKeyboardColor.POSITIVE)
+                write_msg(event.user_id, f"Хай, {event.user_id}. Хоте ли бы вы познакомиться с новыми людьми? нажмите start", keyboard)
+            elif request == "start":
+                n_keyboard = VkKeyboard()
+                n_keyboard.add_button('next', VkKeyboardColor.PRIMARY)
+                n_keyboard.add_button('elect', VkKeyboardColor.POSITIVE)
+                n_keyboard.add_button('ignore', VkKeyboardColor.NEGATIVE)
+                write_msg(event.user_id, f'Для перехода к следующему профилю нажмите next', n_keyboard)
                 vk_ex.message_send_photo(event.user_id, bottoken)#вызов функции и вывод 3 человек в сообщении
-            elif request == "следующий":
+            elif request == "next":
                 vk_ex.message_send_photo(event.user_id, bottoken)
+            elif request == "elect":
+                # МЕТОД ДОБАВЛЕНИЯ В ИЗБРАННЫЕ
+                pass
+            elif request == "ignor":
+                #МЕТОД ДОБАВЛЕНИЯ В ЧЕРНЫЙ СПИСОК
+                pass
             elif request == "нет":
                 write_msg(event.user_id, f"Это чат для знакомств, нам больше нечего предложить, досвидания")
             elif request == "пока":
